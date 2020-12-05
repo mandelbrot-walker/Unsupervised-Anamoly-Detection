67383c4ef215bc8c4cd9a01afdea0817b098571f
zahinahmed97 <zahin.ahmed@northsouth.edu>
2020-12-06 02:58:15 +0600

    more plot choices

--- Final Methodology/P2P/Aug8-Done/plotcode.R
+++ Final Methodology/P2P/Aug8-Done/plotcode.R
@@ -1,7 +1,8 @@
 library(svglite)
 library(ggplot2)
 library(reshape2)
-
+library(ggthemes)
+library("ggsci")
 cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
 melted_cormat <- melt(c)
 head(melted_cormat)
@@ -26,14 +27,24 @@ upper_tri
 melted_cormat <- melt(upper_tri, na.rm = TRUE)
 # Heatmap
 
+
 ggplot(data = melted_cormat, aes(Var2, Var1, fill = value))+
   geom_tile(color = "white")+
-  scale_fill_gradient2(low = "#999999", high = "#CC79A7", mid = "white", 
-                       midpoint = 0, limit = c(-1,1), space = "Lab", 
-                       name="Pearson\nCorrelation") +
-  theme_minimal()+ 
+  scale_fill_gradient2(midpoint = 0, limit = c(-1,1), space = "Lab", 
+                       name="Pearson\nCorrelation") +scale_fill_viridis(option="cividis")+
+  coord_fixed()+theme_minimal()+ 
   theme(axis.text.x = element_text(angle = 45, vjust = 1, 
-                                   size = 12, hjust = 1))+
-  coord_fixed()
+                                   size = 12, hjust = 1))
 
 ggsave(file="test.svg", dpi= "print")
+
+
+ggplot(data = melted_cormat, aes(Var2, Var1, fill = value))+
+  geom_tile(color = "white")+
+  scale_fill_gradient2(midpoint = 0, limit = c(-1,1), space = "Lab", 
+                       name="Pearson\nCorrelation") +scale_fill_material("brown")+
+  coord_fixed()+theme_minimal()+ 
+  theme(axis.text.x = element_text(angle = 45, vjust = 1, 
+                                   size = 12, hjust = 1))
+
+ggsave(file="test2.svg", dpi= "print")
